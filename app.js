04f44f28dfa48bc5ef07ae330e9879c147aef057
Guilherme Luis <guile.hm@hotmail.com>
2019-07-21 21:05:31 -0300

    require mongoose

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const morgan = require('morgan')
 const helmet = require('helmet')
 
 const DEBUG = process.env.DEBUG
 const app = new express()
+
+const env = process.env.NODE_ENV || 'development'
+const mongoConfig = require('./config/mongo')[env]
+const envUrl = process.env[mongoConfig.use_env_variable]
+const DevUrl = `mongodb://${mongoConfig.host}/${mongoConfig.database}`
+const mongoUrl = envUrl ? envUrl : DevUrl
+
+mongoose
+    .connect(mongoUrl, { useNewUrlParser: true })
+    .then(() => console.log('Connected to Mongo'))
+    .catch(e => console.log('Something went wrong', e))
+
 
 app.use(morgan('short'))
 app.use(helmet())
